20de9c544833d54386bc75f8f1a100101d855421
Giuliano Lins <linsgiuliano@gmail.com>
2021-04-29 15:30:04 -0300

    Implementing hrpayroll mock

---
+++ hr-payroll/src/main/java/com/humanresource/hrpayroll/services/PaymentService.java
@@ -0,0 +1,16 @@
+package com.humanresource.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.humanresource.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		
+		return new Payment("Bob", 200.0, days);
+		
+	}
+	
+}
