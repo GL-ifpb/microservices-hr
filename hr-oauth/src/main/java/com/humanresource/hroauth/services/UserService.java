9ab582000df9b5b1d7d267e01757f445362df643
Giuliano Lins <linsgiuliano@gmail.com>
2021-05-23 11:23:21 -0300

    Implementing oauth

--- hr-oauth/src/main/java/com/humanresource/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/humanresource/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.humanresource.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.humanresource.hroauth.entities.User;
 import com.humanresource.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -28,6 +31,20 @@ public class UserService {
 		
 		return user;
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email found: " + username);
+		
+		return user;
 	}
 	
 }
